3fb59bf0a21028ff4052a276e02f20dbe7bdded8
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2026-03-07 16:28:49 +0200

    Explaining compare and modulo functions in math node in shader editor

---
+++ Marh_modulo.py
@@ -0,0 +1,2 @@
+for n in range (0, 101):
+    print (f"{n % 0.3:.3f}")
---
+++ compare_node.py
@@ -0,0 +1,10 @@
+value = 1
+epsilon = 0.2
+
+a = 0.85
+
+if (value - epsilon) <= a <= (value + epsilon):
+    print("Yes")
+else:
+    print("No")
+
